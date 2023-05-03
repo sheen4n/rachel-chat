d6cabc28479a9da2d67cbba663d9c010afb34c83
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2023-05-03 17:31:17 +0800

    update gitingore

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
 
+# Custom Function Imports
+# ...
+
+# Initiate App
 app = FastAPI()
+
+# CORS - Origin
 
 
 @app.get("/")
