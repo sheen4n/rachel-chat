838401df8219b5eeab5b17e487f9ff6fb8152f5b
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2023-05-06 19:35:38 +0800

    Called Whisper API

---
+++ backend/functions/openai_requests.py
@@ -0,0 +1,19 @@
+import openai
+from decouple import config
+
+# Retrieve Environment Varaibles
+openai.organization = config("OPEN_AI_ORG")
+openai.api_key = config("OPEN_AI_KEY")
+
+# Open AI - Whisper
+# Convert Audio To Text
+
+
+def convert_audio_to_text(audio_file):
+    try:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        message_text = transcript["text"]
+        return message_text
+    except Exception as e:
+        print(e)
+        return
--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,53 @@ from decouple import config
 import openai
 
 # Custom Function Imports
-# ...
+from functions.openai_requests import convert_audio_to_text
 
 # Initiate App
 app = FastAPI()
 
-# CORS - Origin
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:4174",
+    "http://localhost:3000",
+]
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/health")
+async def check_health():
+    return {"message": "Healthy"}
+
+
+@app.get("/post-audio-get/")
+async def get_audio():
+
+    # Get saved audio
+    audio_input = open("voice.mp3", "rb")
+
+    # Decode audio
+    message_decoded = convert_audio_to_text(audio_input)
+
+    print(message_decoded)
+
+    return "Done"
+# Post bot response
+# Note: Not playing in browser when using post request
+# @app.post("/post-audio/")
+# async def post_audio(file: UploadFile = File(...)):
+#     print("hello")
+
+
+# Run app with main:app --reload for development
+# access docs with http://localhost:8000/docs
